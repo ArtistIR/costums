8ad91bd9633742ebfe54dca7e36d6d555adfd725
ArtistIR <elpadlo.onelove@gmail.com>
2022-10-22 19:25:39 +0300

    err_page

--- js/slider.js
+++ js/slider.js
@@ -26,3 +26,60 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#slider").owlCarousel();
     });
+
+$(document).ready(function(){
+        const slider = $("#slider_2").owlCarousel({
+            loop:true,
+            nav:true,
+            center:true,
+            dots:false,
+            items:5,
+            checkVisible:false,
+            margin:96,
+            autoWidth:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+    });
+    
+    $(document).ready(function(){
+        $("#slider_2").owlCarousel();
+        });
+
+
+        $(document).ready(function(){
+            const slider = $("#slider_3").owlCarousel({
+                loop:true,
+                nav:true,
+                center:true,
+                dots:false,
+                items:5,
+                checkVisible:false,
+                margin:96,
+                autoWidth:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            });
+        });
+        
+        $(document).ready(function(){
+            $("#slider_3").owlCarousel();
+            });
